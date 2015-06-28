d4b8429db4aa86a8db91bf3b534917a5958c5fb0
Gaudy <gbm2494@hotmail.com>
2015-06-28 13:07:39 -0600

    clase para variables globales

---
+++ Smart/Smart/GlobalVar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart
+{
+    public static class GlobalVar
+    {
+        public string cedulaUsuarioActual = "";
+        public int idSucursalActual;
+    }
+}
